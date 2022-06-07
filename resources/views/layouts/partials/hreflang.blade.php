ddb67dec1b632288b45e9546b0281290351fc514
Yura <yura.chernobay@gmail.com>
2022-06-07 18:52:33 +0300

    Fix hreflang atributes

--- resources/views/layouts/partials/hreflang.blade.php
+++ resources/views/layouts/partials/hreflang.blade.php
@@ -1,6 +1,6 @@
 @if( app()->getLocale() == 'ru' )
-  <link rel="alternate" href="{{config('app.url')}}/uk{{$_SERVER['REQUEST_URI']}}" hreflang="uk-ua" />
+  <link rel="alternate" href="{{config('app.url')}}/uk{{$_SERVER['REQUEST_URI']}}" hreflang="uk" />
 @else
-  <link rel="alternate" href="{{config('app.url')}}{{substr($_SERVER['REQUEST_URI'], 3)}}" hreflang="ru-ua" />
+  <link rel="alternate" href="{{config('app.url')}}{{substr($_SERVER['REQUEST_URI'], 3)}}" hreflang="ru" />
 @endif
-<link rel="alternate" href="{{url()->current()}}" hreflang="{{app()->getLocale()}}-ua" />
+<link rel="alternate" href="{{url()->current()}}" hreflang="{{app()->getLocale()}}" />
